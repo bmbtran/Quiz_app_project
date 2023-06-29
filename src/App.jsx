1e284ad49a70fca08b3dfcdc03432917b2f9cf38
Tran Bui <tranbuimddv@gmail.com>
2023-06-29 10:32:16 +0700

    add routers, and make quiz page fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from './Views/Homepage/Homepage.jsx'
 import QuizDetailsView from './Views/QuizDetailsView/QuizDetailsView.jsx'
 import Quiz from './Views/QuizView/Quiz.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import NoPage from './Views/NoPage.jsx'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" exact element={<Homepage/>} />
             <Route path="/course/:id" element={<QuizDetailsView/>} />
             <Route path="/course/:id/quiz" element={<Quiz/>} />
+            <Route path="*" element={<NoPage />} />
           </Routes>
         </Router>
     </div>
